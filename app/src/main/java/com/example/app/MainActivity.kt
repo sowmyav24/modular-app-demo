687b3321f2eee16558cba88811e4de55170976a4
Sowmya Viswanathan <sowmyavi@thoughtworks.com>
2020-12-13 07:39:31 +0530

    Add navigation for cart

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cart.ui.CartActivity
 import com.example.product.ui.ProductActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
@@ -27,9 +31,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
     private fun startProductActivity(category: String) {
         val intent = Intent(this, ProductActivity::class.java)
         intent.putExtra("category", category)
         startActivity(intent)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.nav_cart -> {
+                startActivity(Intent(this, CartActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
