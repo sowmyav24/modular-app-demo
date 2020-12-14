9ad0d54379070c716e6a39c17cb2fee1e8f7af86
Sowmya Viswanathan <sowmyavi@thoughtworks.com>
2020-12-14 15:05:58 +0530

    Show cart badge in home page

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -5,13 +5,24 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cart.service.CartService
 import com.example.cart.ui.CartActivity
 import com.example.product.ui.ProductListActivity
+import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.main_activity.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    var textCartItemCount: TextView? = null
+
+    @Inject
+    lateinit var cartService: CartService
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
@@ -31,10 +42,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        cartCount()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
+        inflater.inflate(R.menu.product_menu, menu)
+        val menuItem = menu.findItem(R.id.action_cart)
+        val actionView: View = menuItem.actionView
+        textCartItemCount = actionView.findViewById<View>(R.id.cart_badge) as TextView
+        cartCount()
+        actionView.setOnClickListener { onOptionsItemSelected(menuItem) }
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_cart -> {
+                startActivity(Intent(this, CartActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun startProductActivity(category: String) {
@@ -43,13 +74,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.nav_cart -> {
-                startActivity(Intent(this, CartActivity::class.java))
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun cartCount() {
+        textCartItemCount?.text = cartService.getCartCount().toString()
     }
+
 }
